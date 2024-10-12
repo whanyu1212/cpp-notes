00d44a85b757cb590e72f98371b7391927635fbf
Hanyu Wu <whanyu1212@hotmail.com>
2024-10-12 13:10:39 +0800

    added comments for NcR formula

--- src/fe5226/practice_quiz/pascal_triangle_2.cpp
+++ src/fe5226/practice_quiz/pascal_triangle_2.cpp
@@ -9,6 +9,7 @@ void pascal(size_t n, size_t *dst)
     // The first value in a row is always 1
     dst[0] = 1;
     for (size_t j = 1; j <= n; j++)
+        // n choose k = n choose (k-1) * (n+1-k) / k
         dst[j] = dst[j - 1] * (n + 1 - j) / j;
 }
 
